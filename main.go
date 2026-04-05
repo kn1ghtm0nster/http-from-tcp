4147af94bc435c5d02d58a1a2d7d9cf208edb790
kn1ghtm0nster <diego.quintanilla@proton.me>
2026-04-05 18:43:55 -0400

    CH1-L3: update main.go to read dummy files 8 bytes at a time

--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 
 func main () {
-	fmt.Println("I hope I get the job!")
+	byteData, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		return
+	}
+
+	defer byteData.Close()
+
+	// while there is data to read from the file, read it and print it to the console
+	// 8 bytes at a time
+	fileBuffer := make([]byte, 8)
+	for {
+		n, err := byteData.Read(fileBuffer)
+		if err != nil {
+			if err.Error() == "EOF" {
+				return
+			}
+			fmt.Printf("Error reading file: %v\n", err)
+			return
+		}
+		fmt.Printf("read: %s\n", string(fileBuffer[:n]))
+	}
 }
