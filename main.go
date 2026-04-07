ad0a03bc8dd0cc053aee433b61b50ddf9e49aa39
kn1ghtm0nster <diego.quintanilla@proton.me>
2026-04-06 20:55:57 -0400

    CH1-L5: refactor main.go; adds new channel reader func

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"os"
-	"strings"
 )
 
 
@@ -16,38 +13,8 @@ func main () {
 		return
 	}
 
-	defer byteData.Close()
-
-	// variable to hold contents of current line 
-	currentLine := ""
-
-	// while there is data to read from the file, read it and print it to the console
-	// 8 bytes at a time
-	fileBuffer := make([]byte, 8)
-	for {
-		n, err := byteData.Read(fileBuffer)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				if len(currentLine) > 0 {
-					fmt.Printf("read: %s\n", currentLine)
-					currentLine = ""
-				}
-				return
-			}
-			fmt.Printf("Error reading file: %v\n", err)
-			return
-		}
-
-		str := string(fileBuffer[:n])
-
-		parts := strings.Split(str, "\n")
-		for i, part := range parts {
-			if i == len(parts)-1 {
-				currentLine += part
-			} else {
-				fmt.Printf("read: %s\n", currentLine + part)
-				currentLine = ""
-			}
-		}
+	lines := getLinesChannel(byteData)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 }	
