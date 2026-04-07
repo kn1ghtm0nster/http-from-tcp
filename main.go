28a70b4f39321db5a7af09fdf9bea4b433336471
kn1ghtm0nster <diego.quintanilla@proton.me>
2026-04-06 20:28:35 -0400

    CH1-L4: add state machine logic to main.go

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 
@@ -15,18 +18,36 @@ func main () {
 
 	defer byteData.Close()
 
+	// variable to hold contents of current line 
+	currentLine := ""
+
 	// while there is data to read from the file, read it and print it to the console
 	// 8 bytes at a time
 	fileBuffer := make([]byte, 8)
 	for {
 		n, err := byteData.Read(fileBuffer)
 		if err != nil {
-			if err.Error() == "EOF" {
+			if errors.Is(err, io.EOF) {
+				if len(currentLine) > 0 {
+					fmt.Printf("read: %s\n", currentLine)
+					currentLine = ""
+				}
 				return
 			}
 			fmt.Printf("Error reading file: %v\n", err)
 			return
 		}
-		fmt.Printf("read: %s\n", string(fileBuffer[:n]))
+
+		str := string(fileBuffer[:n])
+
+		parts := strings.Split(str, "\n")
+		for i, part := range parts {
+			if i == len(parts)-1 {
+				currentLine += part
+			} else {
+				fmt.Printf("read: %s\n", currentLine + part)
+				currentLine = ""
+			}
+		}
 	}
-}
+}	
