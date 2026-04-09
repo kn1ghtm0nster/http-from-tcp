bc8d204c2eeb7b45c1a5569944646efa3c32cd8a
kn1ghtm0nster <diego.quintanilla@proton.me>
2026-04-09 19:50:38 -0400

    CH4-L2: add request-line parsing and starting test suite

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,72 @@
 package request
+
+import (
+	"errors"
+	"io"
+	"strings"
+	"unicode"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	byteData, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	// parsed data here via custom parseRequestLine function
+	requestLine, err := parseRequestLine(byteData)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Request{
+		RequestLine: requestLine,
+	}, nil
+}
+
+func parseRequestLine(data []byte) (RequestLine, error) {
+	parts := strings.Split(string(data), "\r\n")
+	// discard everything after the first line
+	if len(parts) == 0 {
+		return RequestLine{}, errors.New("empty request")
+	}
+
+	// separate out the request-line into parts by spaces
+	requestLineParts := strings.Split(parts[0], " ")
+	if len(requestLineParts) != 3 {
+		return RequestLine{}, errors.New("invalid request format")
+	}
+
+	method := requestLineParts[0]
+	if method == "" {
+		return RequestLine{}, errors.New("no method specified in request")
+	}
+	// ensure that method is composed entirely of uppercase letters
+	for _, c := range method {
+		if !unicode.IsUpper(c) {
+			return RequestLine{}, errors.New("invalid method format")
+		}
+	}
+
+	// ensure that the HTTP version is strictly HTTP/1.1
+	httpVersion := requestLineParts[2]
+	if httpVersion != "HTTP/1.1" {
+		return RequestLine{}, errors.New("unsupported HTTP version")
+	}
+
+	return RequestLine{
+		Method:        requestLineParts[0],
+		RequestTarget: requestLineParts[1],
+		HttpVersion:   strings.TrimPrefix(requestLineParts[2], "HTTP/"),
+	}, nil
+}
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,11 +1,73 @@
 package request
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRequestLineParse(t *testing.T) {
-	assert.Equal(t, "TheTestagen", "TheTestagen")
+	// Test: Good GET Request line
+	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Good GET Request line with path
+	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Good POST request with path
+	r, err = RequestFromReader(strings.NewReader("POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+
+	// Test: Invalid number of parts in request line
+	_, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: Invalid method (out of order)
+	_, err = RequestFromReader(strings.NewReader("/coffee GET HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: Unsupported HTTP version
+	_, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/2.0\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+}
+
+func TestParseRequestLine(t *testing.T) {
+	// Test: Good Request line
+	requestLine, err := parseRequestLine([]byte("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, requestLine)
+	assert.Equal(t, "GET", requestLine.Method)
+	assert.Equal(t, "/coffee", requestLine.RequestTarget)
+	assert.Equal(t, "1.1", requestLine.HttpVersion)
+
+	// Test: Empty request line
+	_, err = parseRequestLine([]byte("\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: Too many parts in request line
+	_, err = parseRequestLine([]byte("GET /coffee HTTP/1.1 EXTRA\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: Missing method in request line
+	_, err = parseRequestLine([]byte("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: Invalid method in request line
+	_, err = parseRequestLine([]byte("pOsT /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+	// Test: Unsupported HTTP version in request line
+	_, err = parseRequestLine([]byte("GET /coffee HTTP/2.0\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
 }
