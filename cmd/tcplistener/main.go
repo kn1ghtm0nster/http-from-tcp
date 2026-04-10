58b5652d8ddec2a5c683356fb55363f307b0a26e
kn1ghtm0nster <diego.quintanilla@proton.me>
2026-04-10 18:34:36 -0400

    CH4-L6: remove getLinesChannel func from project and use RequestFromReader instead

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net"
+
+	"github.com/kn1ghtm0nster/http-from-tcp/internal/request"
 )
 
 
@@ -22,10 +24,14 @@ func main () {
 		}
 		
 		fmt.Println("NEW CONNECTION ACCEPTED!")
-		linesCh := getLinesChannel(connection)
-		for line := range linesCh {
-			fmt.Println(line)
+		req, err := request.RequestFromReader(connection)
+		if err != nil {
+			log.Printf("ERROR READING REQUEST: %v", err)
+			connection.Close()
+			continue
 		}
+		fmt.Println("Request line:")
+		fmt.Printf("- Method: %s\n- Target: %s\n- Version: %s\n", req.RequestLine.Method, req.RequestLine.RequestTarget, req.RequestLine.HttpVersion)
 		fmt.Println("CONNECTION CLOSED!")
 	}
 }
